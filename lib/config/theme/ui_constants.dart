02a5c593f1d0f047f90b416cf3036586a75fdebe
DmGvozd <mihailzubienko@gmail.com>
2026-04-03 21:36:13 +0300

    chore: new padding constants

--- lib/config/theme/ui_constants.dart
+++ lib/config/theme/ui_constants.dart
@@ -3,6 +3,9 @@ class UiConstants {
   static const double rightPadding = 8;
   static const double bottomPadding = 8;
   static const double topPadding = 8;
+  static const double middlePadding = 8;
 
   static const double borderRadius = 2;
+
+  static const double boxUnit = 8;
 }
