b39beedb95c225ea6302a02d7086c76fda621f3d
axonde <axonde@macbook.bomb>
2026-04-03 16:03:15 +0300

    chore: init ui constants

---
+++ lib/config/theme/ui_constants.dart
@@ -0,0 +1,8 @@
+class UiConstants {
+  static const double leftPadding = 8;
+  static const double rightPadding = 8;
+  static const double bottomPadding = 8;
+  static const double topPadding = 8;
+
+  static const double borderRadius = 2;
+}
