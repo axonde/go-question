e830920f1530a46d3852a113095d2bdfebab7f1e
axonde <axonde@macbook.bomb>
2026-04-03 11:07:01 +0300

    feat: font upload and theme customization

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF33B4FF);
+  static const Color primaryVariant = Color(0xFF1C4C8A);
+  static const Color secondary = Color(0xFFFFC00F);
+  static const Color secondaryVariant = Color(0xFFEC7E00);
+
+  static const Color background = Colors.transparent;
+  static const Color surface = Color(0xFF07377F);
+
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFFABAAAA);
+
+  static const Color success = Color(0xFF09B103);
+  static const Color error = Color(0xFFFE4450);
+}
