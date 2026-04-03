163286a05a4040696c81537da15d743f5814275c
axonde <axonde@macbook.bomb>
2026-04-03 15:06:58 +0300

    fix: replace files in config/theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:go_question/core/theme/app_colors.dart';
-import 'package:go_question/core/theme/app_spacing.dart';
-import 'package:go_question/core/theme/app_text_styles.dart';
+import 'package:go_question/config/theme/app_colors.dart';
+import 'package:go_question/config/theme/app_spacing.dart';
+import 'package:go_question/config/theme/app_text_styles.dart';
 
 class AppTheme {
   static ThemeData main() {
