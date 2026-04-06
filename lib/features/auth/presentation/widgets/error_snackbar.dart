924802199f49c66430d0d4dffe52ddcb72d42406
retrobich921 <albert1yandex@gmail.com>
2026-04-06 15:14:04 +0300

    feat: добавлена защита слоев через проверку сети (NetworkInfo) closes #79

--- lib/features/auth/presentation/widgets/error_snackbar.dart
+++ lib/features/auth/presentation/widgets/error_snackbar.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ErrorSnackBar extends StatelessWidget {
   final String message;
 
-  const ErrorSnackBar({required this.message});
+  const ErrorSnackBar({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
